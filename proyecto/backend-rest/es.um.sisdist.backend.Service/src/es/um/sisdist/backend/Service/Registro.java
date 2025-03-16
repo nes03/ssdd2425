1d047c582f612f3df2fc98e7879a8dd977ccafdf
corncakethepie <alexgomez.soriano@gmail.com>
2025-03-16 19:12:11 +0100

    Registrar hehco(creo)

---
+++ proyecto/backend-rest/es.um.sisdist.backend.Service/src/es/um/sisdist/backend/Service/Registro.java
@@ -0,0 +1,31 @@
+package es.um.sisdist.backend.Service;
+
+
+import es.um.sisdist.backend.Service.impl.AppLogicImpl;
+import es.um.sisdist.models.UserDTO;
+import es.um.sisdist.models.UserDTOUtils;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+// POJO, no interface no extends
+
+@Path("/Registro")
+public class Registro
+{
+    private AppLogicImpl impl = AppLogicImpl.getInstance();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response registerUser(String email, String password, String name){
+        // Crear el objeto UserDTO correctamente
+        UserDTO user = new UserDTO(email, email, password, name, "prueba", 0);
+        return Response.ok(user).build();
+    }
+}
+
